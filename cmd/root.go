34c962a993024dd1d9c4ee77c767557e0fb0deea
Zac Blazic <zacblazic@gmail.com>
2018-05-18 00:25:18 +0200

    Simplify fatal error

--- cmd/root.go
+++ cmd/root.go
@@ -21,8 +21,6 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -45,8 +43,7 @@ var rootCmd = &cobra.Command{
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 }
 
